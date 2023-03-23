57fd7c6188ad32d307bd25a76e9029cab7f77cc2
yjzhang <yjzhang@cs.washington.edu>
2023-03-22 22:27:01 -0700

    updated tests

--- test/test_graphs.py
+++ test/test_graphs.py
@@ -18,8 +18,14 @@ class GraphTest(unittest.TestCase):
     def test_hypergeom(self):
         df = kgfe.load_graph('kegg_pathway_data.csv')
         graph = kgfe.df_to_networkx(df)
-        kgfe.explanations.hypgergeom_test(graph, [7124, 3667, 3630], 'Gene')
+        hg_results = kgfe.explanations.hypgergeom_test(graph, [7124, 3667, 3630], 'Gene')
+        self.assertTrue(len(hg_results) > 0)
 
     def test_pagerank(self):
         df = kgfe.load_graph('kegg_pathway_data.csv')
         graph = kgfe.df_to_networkx(df)
+        pr_results = kgfe.explanations.topic_pagerank(graph, [6504, 5972, 958, 100, 8797, 26762], 'Gene')
+        self.assertTrue(len(pr_results) == len(graph.nodes))
+
+if __name__ == '__main__':
+    unittest.main()
