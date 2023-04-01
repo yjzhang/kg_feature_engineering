f9b1430c5fcff264a9f6707acf3003919d3bb40a
Yue Zhang <yjzhang@cs.washington.edu>
2023-03-31 17:03:38 -0700

    msigdb processor?

--- download_msigdb.py
+++ download_msigdb.py
@@ -1,11 +1,43 @@
 # TODO: process msigdb into a single formatted csv (or a collection of csvs)
 
 # data source: http://www.gsea-msigdb.org/gsea/msigdb/human/collections.jsp
+import json
+import os
 
-files = ['c1.all.v2023.1.Hs.json',
-         'c2.all.v2023.1.Hs.json',
-         'c3.all.v2023.1.Hs.json',
-         'c4.all.v2023.1.Hs.json',
-         'c5.all.v2023.1.Hs.json',
-         'c6.all.v2023.1.Hs.json',
-         ]
+import gene_names
+
+BASE_PATH = '/media/yjzhang/easystore-5gb-1/research_big_data/msigdb/msigdb_v2023.1.Hs_json_files_to_download_locally'
+files = [x for x in os.listdir(BASE_PATH) if x.endswith('.json')]
+# TODO: object categories for every msigdb category, also need predicates and object_id_prefix
+files_to_object_category = {}
+for f in files:
+    pass
+
+def load_file(json_filename, object_category='BiologicalProcess', object_id_prefix='MSigDB',
+        predicate='participates_in'):
+    with open(json_filename) as f:
+        data = json.load(f)
+    new_entries = []
+    for k, v in data.items():
+        gene_set_name = k
+        source = v['exactSource']
+        ref = v['pmid']
+        genes = v['geneSymbols']
+        gene_ids = gene_names.get_ids(genes)
+        for gene_id, symbol in zip(gene_ids, genes):
+            entry = {}
+            entry['subject_category'] = 'Gene'
+            entry['subject_id_prefix'] = 'NCBIGene'
+            entry['subject_id'] = gene_id
+            entry['subject_name'] = symbol
+            entry['predicate'] = predicate
+            entry['object_category'] = object_category
+            entry['object_id_prefix'] = object_id_prefix
+            entry['object_id'] = gene_set_name
+            entry['object_name'] = gene_set_name
+            entry['Primary_Knowledge_Source'] = source
+            entry['Knowledge_Source'] = 'KEGG_Pathways'
+            entry['publications'] = ref
+            new_entries.append(entry)
+    return new_entries
+
