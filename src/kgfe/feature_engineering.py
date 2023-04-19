64e22d8df9b9db72a4a4a424c21b705cec416eb2
yjzhang <yjzhang@cs.washington.edu>
2023-04-18 19:47:51 -0700

    feature engineering

--- src/kgfe/feature_engineering.py
+++ src/kgfe/feature_engineering.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import pandas as pd
-from .graph_info import get_nodes_table
+from .graph_info import get_names_to_ids
 
 def get_feature_pairs(graph, ids, category='Gene'):
     """
@@ -15,15 +15,37 @@ def get_feature_pairs(graph, ids, category='Gene'):
     return edges
 
 
-def generate_pairwise_features(df, graph, is_name=True, category='Gene', mode='avg'):
+def generate_pairwise_features(df, graph, is_name=True, mode='avg'):
     """
     Generates a new dataframe with pairwise features from the graph...
-    """
-    # TODO
-    genes = df.columns
-    graph_nodes = get_nodes_table(graph)
 
-def generate_gene_set_features(df, graph, is_name=True, category='Gene',
+    mode: 'avg', 'product'
+    """
+    genes = df.columns
+    if is_name:
+        names_to_ids = get_names_to_ids(graph)
+        target_ids = [names_to_ids[n] for n in genes]
+    else:
+        target_ids = genes
+    pairs = set()
+    for gene_id in target_ids:
+        neighbors = graph.neighbors(gene_id)
+        for n in neighbors:
+            if n in genes and (n, gene_id) not in pairs:
+                pairs.add((gene_id, n))
+        pass
+    df_new = df.copy()
+    for p in pairs:
+        if mode == 'product':
+            df_new[str(p[0]) + '-x-' + str(p[1])] = df[p[0]]*df[p[1]]
+        elif mode == 'avg' or mode == 'average' or mode == 'mean':
+            df_new[str(p[0]) + '-x-' + str(p[1])] = (df[p[0]]+df[p[1]])/2
+        elif mode == 'harmonic':
+            df_new[str(p[0]) + '-x-' + str(p[1])] = 2/(1/df[p[0]] + 1/df[p[1]])
+    return pairs, df_new
+
+
+def generate_gene_set_features(df, graph, is_name=True,
         target_category='BiologicalProcess', mode='avg', included_gene_sets=None):
     """
     Generates a new dataframe with new features representing groups of genes (all genes connected to genes in the df)
@@ -32,14 +54,40 @@ def generate_gene_set_features(df, graph, is_name=True, category='Gene',
         df - pandas DataFrame containing the data
         graph -  networkx graph
         is_name - whether the inputs are gene names
+        category - usually one of 'Gene',
 
     Returns a new dataframe
     """
     # TODO
-    # 1. get all BPs
     genes = df.columns
-    graph_nodes = get_nodes_table(graph)
-    # 2. for all BPs in included_gene_sets, identify their neighboring genes
+    if is_name:
+        names_to_ids = get_names_to_ids(graph)
+        target_ids = [names_to_ids[n] for n in genes]
+    else:
+        target_ids = genes
+    gene_sets_to_genes = {}
+    genes_to_gene_sets = {}
+    for gene_id in target_ids:
+        neighbors = graph.neighbors(gene_id)
+        genes_to_gene_sets[gene_id] = set()
+        for n in neighbors:
+            if graph.nodes[n]['category'] == target_category:
+                if n in gene_sets_to_genes:
+                    gene_sets_to_genes[n].add(gene_id)
+                else:
+                    gene_sets_to_genes[n] = set([gene_id])
+                genes_to_gene_sets[gene_id].add(n)
+    df_new = df.copy()
+    for gs, genes in gene_sets_to_genes.items():
+        # TODO: implement merging
+        if mode == 'product':
+            df_new[gs] = df[genes]
+        elif mode == 'avg' or mode == 'average' or mode == 'mean':
+            df_new[gs] = (df[genes])/2
+        elif mode == 'harmonic':
+            df_new[gs] = df[genes]
+    return gene_sets_to_genes, df_new
+
 
 def get_repressor_features(graph, ids):
     """
