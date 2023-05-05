de12760d2b479a80c26c45af262ca48f4bfebfe6
yjzhang <yjzhang@cs.washington.edu>
2023-05-04 18:38:41 -0700

    add gene names

---
+++ gene_names.py
@@ -0,0 +1,36 @@
+# extremely simple way of converting gene names
+import gzip
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+ID_TO_SYMBOL = {}
+SYMBOL_TO_ID = {}
+
+
+def _load_gene_info():
+    with gzip.open(os.path.join(base_dir, 'Homo_sapiens.gene_info.gz'), 'rt') as f:
+        for row in f.readlines():
+            row = row.split('\t')
+            if row[0] == '#tax_id':
+                continue
+            row[1] = int(row[1])
+            ID_TO_SYMBOL[row[1]] = row[2]
+            if row[2] not in SYMBOL_TO_ID:
+                SYMBOL_TO_ID[row[2]] = row[1]
+
+
+_load_gene_info()
+    
+
+def get_symbols(gene_ids):
+    """
+    Get symbols given a single gene id or a list of gene ids.
+    """
+    return [ID_TO_SYMBOL[x] for x in gene_ids]
+
+
+def get_ids(gene_symbols):
+    """
+    Get ids given a list of gene symbols.
+    """
+    return [SYMBOL_TO_ID[x] for x in gene_symbols]
