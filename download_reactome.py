cf5f7d13d26fa14954e315a725deae755e2186e1
yjzhang <yjzhang@cs.washington.edu>
2023-05-23 19:52:07 -0700

    starting on id conversions, reactome

--- download_reactome.py
+++ download_reactome.py
@@ -1,1 +1,4 @@
 # TODO: load reactome data
+# download links: https://reactome.org/download-data
+# files: NCBI2Reactome.txt - NCBI gene IDs to reactome lowest-level pathways
+# files: ChEBI2Reactome.txt - ChEBI IDs to reactome lowest-level pathways
---
+++ src/kgfe/togo_id_api.py
@@ -0,0 +1,1 @@
+# API for https://academic.oup.com/bioinformatics/article/38/17/4194/6633929, https://togoid.dbcls.jp/apidoc/
